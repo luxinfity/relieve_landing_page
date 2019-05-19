20e6af5cc724aee03ca48e0f9f0b0feeaff40bf0
alifgiant <muh.alifgiant@gmail.com>
2019-05-19 15:06:30 +0700

    remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
