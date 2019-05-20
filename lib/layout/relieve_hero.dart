80bbb112a26d39ee1a87191427f6da3cc2d90bec
alifgiant <muh.alifgiant@gmail.com>
2019-05-20 16:08:41 +0700

    include image

--- lib/layout/relieve_hero.dart
+++ lib/layout/relieve_hero.dart
@@ -1,12 +1,31 @@
 import 'package:flutter_web/widgets.dart';
 import 'package:relieve_landing_page/res/color.dart';
 
+class HeroContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('Cinta dan keselamatan keluarga dalam satu aplikasi'),
+        Text('“I am aware, we are aware”'),
+      ],
+    );
+  }
+}
+
 class RelieveHero extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      color: AppColor.colorEmptyChip,
+      // height: 300,
+      child: Stack(
+        children: <Widget>[
+          Image.asset('images/overflow-left.png'),
+          Image.asset('images/overflow-right.png'),
+          Image.asset('images/overflow-bottom.png'),
+          HeroContent(),
+        ],
+      ),
     );
   }
 }
